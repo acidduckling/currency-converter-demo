c6481947ee50f2757bc180a1cd5a49a7137108b9
Clayton Wakefield <acidduckling@gmail.com>
2018-07-16 14:38:12 +1000

    SelectCurrency component

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import image from '../images/cash-calculator.svg'
+import image from '../images/cash-calculator.svg';
+
+import data from './data/Data';
+import SelectCurrency from './components/SelectCurrency';
 
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-
+    this.state = {
+      currencies: data.currencies,
+      currencyA: data.currencies[0],
+      currencyB: data.currencies[1],
+      currencyAVal: data.currencies[0].sellRate,
+      currencyBVal: data.currencies[1].sellRate
+    };
   }
-  render(){
+
+  onSelectCurrency = code => {
+    //console.log(code);
+    const { currencies } = this.state;
+    const currency = currencies.filter(currency => currency.code == code)[0];
+    this.setState({
+      currencyB: currency
+    });
+  };
+
+  render() {
+    const { currencies, currencyA, currencyAVal, currencyB, currencyBVal } = this.state;
     return (
       <div>
         <header>
@@ -22,53 +41,68 @@ class App extends React.Component {
                 {
                   //Select currency
                 }
-                <select>
-                  <option value="A">Option A</option>
-                  <option value="B">Option B</option>
-                </select>
+                <SelectCurrency currencies={currencies} onSelectCurrency={this.onSelectCurrency} />
               </p>
             </div>
           </div>
-          
+
           <div className="row">
             <div className="col-sm-6 currency-from-input">
-              <h3 className="currency-flag AUD">Australian Dollars</h3>
+              <h3 className={`currency-flag ${currencyA.code}`}>${currencyA.name}</h3>
               {
-                  //Currency A input
+                //Currency A input
               }
               <div className="input-group">
-                <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon2" step="1" pattern="\d\.\d{2}"  />
-                <span className="input-group-addon" id="basic-addon2">AUD</span>
+                <span className="input-group-addon">${currencyA.sign}</span>
+                <input
+                  type="number"
+                  defaultValue={0}
+                  className="form-control"
+                  aria-describedby="basic-addon2"
+                  step="1"
+                  pattern="\d\.\d{2}"
+                />
+                <span className="input-group-addon" id="basic-addon2">
+                  ${currencyA.code}
+                </span>
               </div>
-
             </div>
             <div className="col-sm-6 currency-to-input">
-              <h3 className="currency-flag USD">United States Dollars</h3>
+              <h3 className={`currency-flag ${currencyB.code}`}>{currencyB.name}</h3>
               {
-                  //Currency B input
+                //Currency B input
               }
               <div className="input-group">
-                <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon3" step="1" pattern="\d\.\d{2}"  />
-                <span className="input-group-addon" id="basic-addon3">USD</span>
+                <span className="input-group-addon">${currencyB.sign}</span>
+                <input
+                  type="number"
+                  defaultValue={0}
+                  className="form-control"
+                  aria-describedby="basic-addon3"
+                  step="1"
+                  pattern="\d\.\d{2}"
+                />
+                <span className="input-group-addon" id="basic-addon3">
+                  {currencyB.code}
+                </span>
               </div>
-
             </div>
           </div>
           <div className="row">
             <div className="col-sm-12">
               {
-                  //Update to currently selected currency
+                //Update to currently selected currency
               }
               <p>
-                Exchange Rate $ 1 AUD = $ 0.7041 USD
+                Exchange Rate
+                {`${currencyA.sign} ${currencyA.sellRate} ${currencyA.code} 
+              = ${currencyB.sign} ${currencyB.sellRate} ${currencyB.code}`}
               </p>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
